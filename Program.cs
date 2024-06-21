eb443556fb89077bf74f9d2ee764c9dad12907e4
Alfred Odey <alfredmodey@gmail.com>
2024-06-22 00:04:08 +0100

    Question 8

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingListMethod
+{
+    internal class Program
+    {
+        /*
+        Question 8: Using List Methods
+        Create a list of strings with the values
+        {"apple", "banana", "cherry"}.
+        Add "date" to the list.
+        Remove "banana" from the list.
+        Check if the list contains "cherry".
+        Print all elements in the list.
+        */
+        static void Main(string[] args)
+        {
+            List<string> fruits = new List<string>
+         {
+             "apple", "banana", "cherry"
+         };
+
+            fruits.Add("date");
+            fruits.Remove("banana");
+
+            if (fruits.Contains("cherry"))
+            {
+                Console.WriteLine(true);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < fruits.Count; i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }
+        }
+    }
+}
